a8c14e48e033d11cd52f32eed5e0f88510e80a0a
BrokenFlows <cianfdowd@gmail.com>
2021-10-10 14:16:30 +0100

    moves some elements out of ishka.keymap and redefines some macros moves combos and defnitions into separate files for easier reading of main keymap

---
+++ config/46-key.h
@@ -0,0 +1,14 @@
+#define XXX &none
+#define ___ &trans
+
+#define BASE    0
+#define FUNC    1
+#define NAV     2
+
+#define MNBR  LC(F2)
+#define REDO  LS(LG(Z))
+#define UNDO  LG(Z)
+#define CUT   LG(X)
+#define COPY  LG(C)
+#define PASTE LG(V)
+
